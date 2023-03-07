0925620ed8731426d2ce73320456c831f14355ee
bodybri <bryanhuang404@gmail.com>
2023-03-06 21:18:21 -0800

    Initial Commit

---
+++ LearingGH/LearingGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearingGHApp.swift
+//  LearingGH
+//
+//  Created by Bryan Huang on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LearingGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
